8433f53bef378febc87f97d9b6fa8d324f6d8309
oskarasbrink <44872607+oskarasbrink@users.noreply.github.com>
2022-07-13 09:09:44 +0200

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,3 +1,9 @@
-#!/bin/bash
+#!/bin/env sh
 
+docker build -t lamastex/dockerdev:latest .
+
+docker run --rm -d -it --name=spark-gdelt --mount type=bind,source=${HOME},destination=/root/GIT lamastex/dockerdev:latest
+
+ls
 pwd
+echo "tjenare mannen"
