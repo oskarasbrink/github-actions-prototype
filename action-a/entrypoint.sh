e7db1fcac924af2a732fb00a11db8b98eb7eb083
oskarasbrink <44872607+oskarasbrink@users.noreply.github.com>
2022-07-13 09:52:49 +0200

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/env sh
 
-docker build -t lamastex/dockerdev:latest .
-
-docker run -v /home/runner/work/github-actions-prototype/github-actions-prototype:/root/GIT lamastex/dockerdev:latest
+echo Hello world my name is mannen
+ls
 
