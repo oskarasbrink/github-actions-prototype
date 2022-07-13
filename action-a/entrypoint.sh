8e46c1b7432a0956ecbbeb6dcbd903ffc56fef79
oskarasbrink <asbrinkoskar@gmail.com>
2022-07-13 12:45:12 +0200

    haskell docker

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
 
-ls /root/
+ls /root/tilowiklund
+
+cd /root/tilowiklund
+mkdir book
+mdbook init .
+mkdir src/contents
+
+
+stack exec pinot -- --from databricks --to mdbook  ~/ASSIGNMENT-1.dbc -o src/contents
+
+
+cd src 
+find contents -iname '*.md' -type f | sort -h | while read f; do echo "- ["$(basename $f .md)"](./$f)"; done > SUMMARY.md
+cd ..
+mdbook build
+
+
 
