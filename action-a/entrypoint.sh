113526c989450eef1065bf90c18277e815f336cc
oskarasbrink <44872607+oskarasbrink@users.noreply.github.com>
2022-07-13 09:47:20 +0200

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -2,8 +2,5 @@
 
 docker build -t lamastex/dockerdev:latest .
 
-docker run --workdir= /root/GIT -v /home/runner/work/github-actions-prototype/github-actions-prototype:/root/GIT lamastex/dockerdev:latest
+docker run -v /home/runner/work/github-actions-prototype/github-actions-prototype:/root/GIT lamastex/dockerdev:latest
 
-ls root/GIT
-pwd
-echo "tjenare mannen"
