303d1f62e572bbfb88c2a6569377dce9b293867e
oskarasbrink <asbrinkoskar@gmail.com>
2022-07-14 09:35:06 +0200

    haskell docker

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -4,6 +4,7 @@
 
 cd /root/tilowiklund/pinot
 chown -R $(id -un):$(id -gn) ~
+echo $HOME
 #TAR_OPTIONS=--no-same-owner stack setup
 #stack build
 #/root/tilowiklund/pinot/.stack-work/install/x86_64-linux-tinfo6/683e847c51fc1564e1993dabfce286242d9677886df9885a79955a2d0adb37f8/8.8.4/bin
