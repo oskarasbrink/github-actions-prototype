979f8625302404c66732d7a98aac9d9429adfab6
oskarasbrink <44872607+oskarasbrink@users.noreply.github.com>
2022-07-14 11:53:08 +0200

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -31,9 +31,11 @@ export PATH=$PATH:/root/tilowiklund/pinot/.stack-work/install/x86_64-linux-tinfo
 #PATH = /root/.cabal/bin:/root/.local/bin:/opt/ghc/9.2.1/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 echo "exec pinot"
 hash -r
+export PATH=$PATH:/root/tilowiklund/pinot/.stack-work/install/x86_64-linux-tinfo6/683e847c51fc1564e1993dabfce286242d9677886df9885a79955a2d0adb37f8/8.8.4/bin
 stack exec pinot -- --from databricks --to mdbook  /github/workspace/ASSIGNMENT-1.dbc -o src/contents
 #PATH=/github/home/.stack/snapshots/x86_64-linux-tinfo6/683e847c51fc1564e1993dabfce286242d9677886df9885a79955a2d0adb37f8/8.8.4/bin:/github/home/.stack/compiler-tools/x86_64-linux-tinfo6/ghc-8.8.4/bin","/github/home/.stack/programs/x86_64-linux/ghc-tinfo6-8.8.4/bin:/root/.cabal/bin:/root/.local/bin:/opt/ghc/9.2.1/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 hash -r
+export PATH=$PATH:/root/tilowiklund/pinot/.stack-work/install/x86_64-linux-tinfo6/683e847c51fc1564e1993dabfce286242d9677886df9885a79955a2d0adb37f8/8.8.4/bin
 stack exec pinot -- --from databricks --to mdbook  /github/workspace/ASSIGNMENT-1.dbc -o src/contents
 echo "."
 ls
