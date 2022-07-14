90392fb82c5aff86ec436802db50ba3c079dac25
oskarasbrink <44872607+oskarasbrink@users.noreply.github.com>
2022-07-14 15:45:55 +0200

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -15,27 +15,13 @@ chown -R $(id -un):$(id -gn) ~
 #TAR_OPTIONS=--no-same-owner stack setup
 #stack build
 #/root/tilowiklund/pinot/.stack-work/install/x86_64-linux-tinfo6/683e847c51fc1564e1993dabfce286242d9677886df9885a79955a2d0adb37f8/8.8.4/bin
-ls
 
-pwd
-echo "whereis mannen"
-whereis pinot
-echo "which mannen"
-which pinot
-cd /root/tilowiklund/pinot
-ls -l
-#cd /root/tilowiklund/pinot
-mkdir -p book
-#mdbook init .
-cd book
-mkdir -p src/contents
-ln -s /github/home /root
-printenv PATH
-echo $HOME
-echo "ls-mannen"
 
-hash -r
 export PATH=$PATH:/root/tilowiklund/pinot/.stack-work/install/x86_64-linux-tinfo6/25209f23054efc632f8f95d62490b13ea73df48993cc9cd44b2ce1348aa04b70/8.8.4/bin
+cd /github/workspace
+mkdir books
+cd books
+mkdir src/contents
 
 stack exec pinot -- --from databricks --to mdbook  /github/workspace/ASSIGNMENT-1.dbc -o ~
 
@@ -43,9 +29,3 @@ stack exec pinot -- --from databricks --to mdbook  /github/workspace/ASSIGNMENT-
 cd src 
 find contents -iname '*.md' -type f | sort -h | while read f; do echo "- ["$(basename $f .md)"](./$f)"; done > SUMMARY.md
 cd ..
-
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -y
-rustc --version
-exec bash
-rustc --version
-cargo install mdbook
