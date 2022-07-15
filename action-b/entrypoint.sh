ca08c8e2970b455c7594d4c8ec3b3caab958561f
oskarasbrink <asbrinkoskar@gmail.com>
2022-07-15 09:40:10 +0200

    trying sh script

--- action-b/entrypoint.sh
+++ action-b/entrypoint.sh
@@ -10,5 +10,6 @@ chmod a+x /github/workspace/action-b/mdscript.sh
 cd /github/workspace/action-b 
 sh mdscript.sh
 
-ls -l
+ls /github/workspace/booksmannen
+cd /github/workspace/booksmannen
 tree .
