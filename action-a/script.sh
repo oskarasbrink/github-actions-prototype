96e0ca3ccb33fb7b27eab08cd37ac0b87bf42c2b
oskarasbrink <asbrinkoskar@gmail.com>
2022-07-15 08:56:47 +0200

    trying sh script

--- action-a/script.sh
+++ action-a/script.sh
@@ -5,8 +5,8 @@ for FILE in /github/workspace/dbcArchives/*; do
     cd /github/workspace
     echo $FILE; 
     #mkdir "book-$FILE"/src/contents
-    mkdir book-"(basename "$FILE" .dbc)"/src/contents
-    cd book-"$(basename "$FILE" .dbc)"
+    mkdir "$(basename "$FILE" .dbc)"/src/contents
+    cd "$(basename "$FILE" .dbc)"
     stack exec pinot -- --from databricks --to mdbook  /github/workspace/dbcArcives/"$FILE" -o src/contents
     cd src
     find contents -iname '*.md' -type f | sort -h | while read f; do echo "- ["$(basename $f .md)"](./$f)"; done > SUMMARY.md
