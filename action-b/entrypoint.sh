39b3e4dc3a99a455f92044fe6889976d41098e5b
oskarasbrink <asbrinkoskar@gmail.com>
2022-07-15 09:27:51 +0200

    trying sh script

--- action-b/entrypoint.sh
+++ action-b/entrypoint.sh
@@ -2,9 +2,11 @@
 
 
 
-cd /github/workspace/books
-mdbook init . -y
-mdbook build
-ls book
-pwd
-
+#cd /github/workspace/books
+#mdbook init . -y
+#mdbook build
+#ls book
+#pwd
+chmod a+x /github/workspace/action-b/mdscript.sh
+cd /github/workspace/action-b 
+sh mdscript.sh
---
+++ action-b/mdscript.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+
+for FILE in /github/workspace/books/*; do
+    cd $FILE
+    mdbook init .
+    mdbook build
+    
